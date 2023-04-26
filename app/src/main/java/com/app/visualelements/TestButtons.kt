c4bd1d844eef3ade60c2cbcc5dc8ce158e4d68c0
IgorMontezuma20 <igorminranda17@gmail.com>
2023-04-25 22:29:12 -0300

    criando buttons

--- app/src/main/java/com/app/visualelements/TestButtons.kt
+++ app/src/main/java/com/app/visualelements/TestButtons.kt
@@ -1,25 +1,36 @@
 package com.app.visualelements
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun TestButtons() {
     val context = LocalContext.current
+    val btnImage = painterResource(id = R.drawable.ic_user)
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Button(onClick = {
-            Toast.makeText(context, "O botão foi clicado", Toast.LENGTH_SHORT).show()
-        }) {
+        Button(
+            onClick = { Toast.makeText(context, "Botão clicado", Toast.LENGTH_SHORT).show() },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Red,
+                contentColor = Color.Green
+
+            )
+            ) {
+            Image(painter = btnImage, contentDescription = null )
             Text(text = "Profile")
         }
     }
